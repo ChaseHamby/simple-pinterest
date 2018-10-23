cf2457a66e055b7ac1f0eb7aa76312d93190667e
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-22 19:35:01 -0500

    added click event for boards

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -1,13 +1,22 @@
 import {loadBoards} from '../data/boardsData.js'
 
+const bindEvents = () => {
+    $('#user-boards').on('click', '.board-card', (e) => {
+        const clickedBoardId = $(e.target).closest('.board-card').attr('id'); // attr targets the id within that specific div id
+        $('#boards-page').hide(); // hiding the boards page when you click it
+        $('pins-page').show(); // show the pins page when you click it
+        console.log(clickedBoardId);
+    })
+}
+
 const writeBoards = (boards) => {
     let domString = '';
     boards.forEach(board => {
         domString += `
-        <div class="board-card p-2">
+        <div id='${board.id}' class="board-card p-2">
             <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">${board.name}</h5>
               <p class="card-text">42 Pins</p>
             </div>
         </div>
@@ -21,6 +30,7 @@ const writeBoards = (boards) => {
 const initializeBoardView = () => {
     loadBoards().then((boards) => {
         writeBoards(boards); // calling the function above
+        bindEvents();
     }).catch((error) => {
         console.error(error);
     })
